761c3337b93cfd9a9239bb0d32ccd1894a51a674
Amir kalid <amirkjaled@gmail.com>
2026-03-11 19:45:12 +0300

    simple error

--- react/src/components/sections/Brands.tsx
+++ react/src/components/sections/Brands.tsx
@@ -12,7 +12,7 @@ const logos = ["paypal-3", "slack", "twitch-purple", "discord-6", "spotify-logo-
             <div className="flex justify-center flex-wrap gap-4">
                 {logos.map((logo, key) => (
                     <div key={key} className="p-4 sm:p-5 rounded-xl bg-body border-box-border group">
-                        <img src={`src/assets/logos/${logo}.svg`} width="100" height="60" className="h-7 sm:h-10 w-auto ease-linear duration-300 grayscale group-hover:!grayscale-0 group-hover:scale-105"></img>
+                        <img src={`assets/logos/${logo}.svg`} width="100" height="60" className="h-7 sm:h-10 w-auto ease-linear duration-300 grayscale group-hover:!grayscale-0 group-hover:scale-105"></img>
                     </div>
                 ))}
             </div>
